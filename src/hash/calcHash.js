283b2bc955b7f974c3650d7c1c74736ca825bcec
romanenkoandrew <ramanenkaandrew@gmail.com>
2025-04-25 18:55:40 +0200

    fix hash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -4,26 +4,15 @@ import { join } from 'path'
 
 const calculateHash = async () => {
     const filePath = join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt')
-    const hash = createHash('sha256')
-    const stream = createReadStream(filePath)
+    const hash = createHash('sha256').setEncoding('hex')
+    const readStream = createReadStream(filePath)
+    
+    readStream
+        .pipe(hash)
+        .pipe(process.stdout)
 
-    return new Promise((resolve, reject) => {
-        stream.on('error', reject)
-        hash.on('error', reject)
-
-        let result = ''
-        hash.setEncoding('hex')
-
-        hash.on('data', chunk => {
-            result += chunk
-        })
-
-        hash.on('end', () => {
-            console.log(result)
-            resolve()
-        })
-
-        stream.pipe(hash)
+    readStream.on('end', () => {
+        process.stdout.write('\n')
     })
 }
 
